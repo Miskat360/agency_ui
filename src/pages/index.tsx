4396d537ee96d7ecab73b9c5e8dd0c42ed305b4a
Miskat Hasan <miskathasan979@gmail.com>
2024-11-28 22:43:18 +0600

    feat/about-us section added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Layout from "@/components/layout/Layout";
 import HomeBanner from "@/components/layout/banner/HomeBanner";
+import Agency from "@/components/containers/home/Agency";
 
 const Home = () => {
   return (
     <Layout video={true}>
       <HomeBanner />
-      {/* <Agency />
-      <PortfolioText />
+      <Agency />
+      {/* <PortfolioText />
       <HomeOffer />
       <HomeTestimonial />
       <HomeBlog />
