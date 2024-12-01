82a26e2fe4060bb7acc30f0687e1122ffa60e2fc
Miskat Hasan <miskathasan979@gmail.com>
2024-12-01 22:25:15 +0600

    feat/protfolio section & images added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import Layout from "@/components/layout/Layout";
 import HomeBanner from "@/components/layout/banner/HomeBanner";
 import Agency from "@/components/containers/home/Agency";
+import PortfolioText from "@/components/containers/home/PortfolioText";
 
 const Home = () => {
   return (
     <Layout video={true}>
       <HomeBanner />
       <Agency />
-      {/* <PortfolioText />
-      <HomeOffer />
-      <HomeTestimonial />
-      <HomeBlog />
-      <HomeSponsor />
-      <NextPage /> */}
+      <PortfolioText />
+      {/* <HomeOffer /> */}
+      {/* <HomeTestimonial /> */}
+      {/* <HomeBlog /> */}
+      {/* <HomeSponsor /> */}
+      {/* <NextPage /> */}
     </Layout>
   );
 };
