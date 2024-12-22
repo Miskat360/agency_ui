568be074ad8fe2ed24e4a88e32732a9e64fb17d0
Miskat Hasan <miskathasan979@gmail.com>
2024-12-22 23:43:00 +0600

    feat/about-us page(banner section) added

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "@/components/layout/Layout";
+import CmnBanner from "@/components/layout/banner/CmnBanner";
+
+const AboutUs = () => {
+  return (
+    <Layout video={0}>
+      <CmnBanner title="About Us" navigation="About Us" />
+      {/* <HomeTwoModal /> */}
+      {/* <Agency /> */}
+      {/* <TeamMembers /> */}
+      {/* <HomeTestimonial /> */}
+      {/* <AboutSponsor /> */}
+      {/* <AboutCta /> */}
+    </Layout>
+  );
+};
+
+export default AboutUs;
