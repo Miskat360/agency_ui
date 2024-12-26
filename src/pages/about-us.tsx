1b6eb00c4e01939b8d6c4dfd6f969b58c7275283
Miskat Hasan <miskathasan979@gmail.com>
2024-12-26 20:23:39 +0600

    feat/modal added in about page

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Layout from "@/components/layout/Layout";
 import CmnBanner from "@/components/layout/banner/CmnBanner";
+import HomeTwoModal from "@/components/containers/home/HomeTwoModal";
 
 const AboutUs = () => {
   return (
     <Layout video={0}>
       <CmnBanner title="About Us" navigation="About Us" />
-      {/* <HomeTwoModal /> */}
+      <HomeTwoModal />
       {/* <Agency /> */}
       {/* <TeamMembers /> */}
       {/* <HomeTestimonial /> */}
